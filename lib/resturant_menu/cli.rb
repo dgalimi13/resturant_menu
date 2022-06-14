bf8163ba869c91fd6347d7af50b74b48f3843b9a
danny galimi <dannygalimi13@aim.com>
2022-06-14 15:15:54 -0400

    added list items method to cli class

--- lib/resturant_menu/cli.rb
+++ lib/resturant_menu/cli.rb
@@ -10,4 +10,13 @@ class ResturantMenu::CLI
          end 
         goodbye
     end
+
+    def list_items
+        puts 'Choose a taco to see its ingredients'
+        sorted = @items.sort { |a, b| a.name <=> b.name }
+        sorted.each_with_index do  |item, index|
+          puts "#{index+1}. #{item.name}"
+        end
+    end
+    
 end 
