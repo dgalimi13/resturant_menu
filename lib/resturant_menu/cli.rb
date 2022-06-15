e7d6ce6f7174022622b8e4d6672c2b8780a94563
danny galimi <dannygalimi13@aim.com>
2022-06-15 18:07:00 -0400

    added show_ingredients_for method

--- lib/resturant_menu/cli.rb
+++ lib/resturant_menu/cli.rb
@@ -18,5 +18,14 @@ class ResturantMenu::CLI
           puts "#{index+1}. #{item.name}"
         end
     end
+
+    def show_ingredients_for(chosen_item)
+        item = @items[chosen_item - 1]
+        item.get_ingredients 
+        puts "Here are the ingredients for #{item.name}"
+        puts item.ingredients[chosen_item - 1].name
+        puts "hit enter to continue"
+        get_user_item
+    end
     
 end 
