cf56eb1d4334fe8f5374c93ef0487eded3d40c96
danny galimi <dannygalimi13@aim.com>
2022-06-29 22:20:36 -0400

    added enviornment file

---
+++ lib/resturant_menu/resturant_menu.rb
@@ -0,0 +1,5 @@
+require_relative "./resturant_menu/version"
+require_relative "./resturant_menu/cli"
+require_relative "./resturant_menu/item"
+require_relative "./resturant_menu/ingredient"
+require_relative "./resturant_menu/scrapper"
