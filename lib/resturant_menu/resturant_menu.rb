570042425e6dec5762cae355b01325b890ea1777
danny galimi <dannygalimi13@aim.com>
2022-06-30 22:01:08 -0400

    added requires and error module to resturant_menu.rb

--- lib/resturant_menu/resturant_menu.rb
+++ lib/resturant_menu/resturant_menu.rb
@@ -3,3 +3,12 @@ require_relative "./resturant_menu/cli"
 require_relative "./resturant_menu/item"
 require_relative "./resturant_menu/ingredient"
 require_relative "./resturant_menu/scrapper"
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+module ResturantMenu
+  class Error < StandardError; end
+  # Your code goes here...
+end
